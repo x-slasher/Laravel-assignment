e81af05e623bb420a849e7a1304b4ccc01fd6789
Abu Osman <>
2020-11-03 12:07:20 +0600

    third commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FindAnswer;
+use App\Jobs\SendEmail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        FindAnswer::class
     ];
 
     /**
@@ -24,7 +26,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+    	$schedule->command('find:answer')->dailyAt('1:00');  //run this schedule at 1 am
+
+	    $schedule->call(function (){
+		    dispatch(new SendEmail());
+	    });
     }
 
     /**
